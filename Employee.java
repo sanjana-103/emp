47d5328f4aafcf09901cbce937aeb78dc9f61766
Sudeep-sm <noobadum00@gmail.com>
2025-05-06 14:51:13 +0530

    save the changes

--- Employee.java
+++ Employee.java
@@ -2,21 +2,24 @@ public class Employee {
     String name;
     int id;
     double salary;
+    int experience;
 
-    public Employee(String name, int id, double salary) {
+    public Employee(String name, int id, double salary, int experience) {
         this.name = name;
         this.id = id;
         this.salary = salary;
+        this.experience = experience;
     }
 
     public void displayInfo() {
         System.out.println("Employee ID: " + id);
         System.out.println("Employee Name: " + name);
         System.out.println("Salary: " + salary);
+        System.out.println("Employee Experience:" +experience);
     }
 
     public static void main(String[] args) {
-        Employee emp1 = new Employee("xyz", 101, 50000.0);
+        Employee emp1 = new Employee("xyz", 101, 50000.0,2);
         emp1.displayInfo();
     }
 }
